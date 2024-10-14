22c6862fadbf91a6b53424210275247644481621
haider ali <haiderali333222@gmail.com>
2024-10-14 22:52:51 +0500

    update for node 20

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 /* eslint-disable react/jsx-wrap-multilines */
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import i18n from './i18n';
-
-import AuthPage from './views/TodoList'
+import AuthPage from './views/TodoList';
 // import UnPrivateRoute from './hoc/UnprivateRoutes';
 
 const App = () => {
-  useEffect(() => {
-    i18n.changeLanguage('en');
-  }, []);
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/" element={<AuthPage path="/signup" />} />
-
-      
         </Routes>
       </Router>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/no-named-as-default */
 import React from 'react';
-import 'antd/dist/antd.css';
 import ReactDOM from 'react-dom';
 
 
